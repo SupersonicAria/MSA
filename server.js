0c4bc1042bfb748e506e056b90ba611c1d91407c
unknown <tavoywalls@gmail.com>
2022-09-30 18:41:29 -0400

    removed catch statement

--- server.js
+++ server.js
@@ -6,15 +6,7 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// SEQUELIZE CONNECTION
 const sequelize = new Sequelize(process.env.PG_URI)
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
 
 const booksController = require('./src/controllers/books_controller')
 app.use('/books', booksController)
